e45dd343872b1f5347f16f6853159e04fcd477c7
wakepon <wakepon@users.noreply.github.com>
2026-01-10 10:47:04 +0000

    deploy: 7403f301b35547c9a745472993bcbf37e08f55f0

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // ゲーム設定
 var CONFIG = {
-    VERSION: 'v2026.01.09.0827',
+    VERSION: 'v2026.01.10.1946',
     // ボードのグリッドサイズ（NxN）
-    BOARD_SIZE: 10,
+    BOARD_SIZE: 5,
     // タッチ操作時のドラッグオフセット(px)
     // ブロックが指で隠れないよう、指の位置より上に表示
     TOUCH_DRAG_OFFSET_Y: 60,
